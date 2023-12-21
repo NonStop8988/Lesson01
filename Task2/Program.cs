ea6dec75cdab4305e61e22c482717f8c4d8ddfa9
Maxim <lyskomax2@mail.ru>
2023-12-21 12:14:57 +0300

    Lesson01:Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿int i1 = 5;
+int i2 = 6;
+int sum1 = i1 + i2;
+Console.WriteLine(sum1);
+
+long l1 = 6;
+long sum2 = l1 + i1;
+Console.WriteLine(sum2);
+
+double d1 = 5.5;
+double sum3 = d1 + i1;
+Console.WriteLine(sum3);
+
+int product1 = i1 * i2;
+long product2 = l1 * i1;
+double product3 = d1 * i1;
+Console.WriteLine(product1);
+Console.WriteLine(product2);
+Console.WriteLine(product3);
