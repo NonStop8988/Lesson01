806ed7f9e0dbcf8190d4bdd9e62096d1cc3b6941
Maxim <lyskomax2@mail.ru>
2023-12-21 11:55:33 +0300

    add some variables lesson01:Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello world");
+﻿// Console.WriteLine("Hello world");
+
+
+int value = 100;
+Console.WriteLine(value);
+
+double d = 5.55;
+char c= '!';
+bool b = true;
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
