d3842c4107a69f79e46c5f122bf84a641591e619
Maxim <lyskomax2@mail.ru>
2023-12-21 12:22:00 +0300

    add Lesson01:Task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -17,3 +17,10 @@ double product3 = d1 * i1;
 Console.WriteLine(product1);
 Console.WriteLine(product2);
 Console.WriteLine(product3);
+
+int quotient = i1 / i2;
+Console.WriteLine(quotient);
+
+d1 = 6;
+double quotient2 = i1 / d1;
+Console.WriteLine(quotient2);
