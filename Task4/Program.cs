9693ba52171bf9f8ecf24510d07149119105c850
Maxim <lyskomax2@mail.ru>
2023-12-21 12:42:07 +0300

    add Lesson01:Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 6;
+int b = 7;
+int c = 9;
+int d = 40;
+int e = 10;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
